35750e93f7de6320b4e5e01bc4ccee275f2bd9f8
Lorena <lorslou@gmail.com>
2023-05-19 12:48:16 +0200

    Added creating new notes functionallity

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -21,6 +21,7 @@ class _NotesViewState extends State<NotesView> {
     super.initState();
   }
 
+  // <-
   @override
   void dispose() {
     _notesService.close();
@@ -73,6 +74,7 @@ class _NotesViewState extends State<NotesView> {
                   builder: (context, snapshot) {
                     switch (snapshot.connectionState) {
                       case ConnectionState.waiting:
+                      case ConnectionState.active:
                         return const Text('Waiting for all notes...');
                       default:
                         return const CircularProgressIndicator();
