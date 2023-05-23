b5621bd318787592b673fe6d9a7fcdaaa7d8f05b
Lorena <lorslou@gmail.com>
2023-05-23 18:11:26 +0200

    Moved to Bloc routing and Dialogs

--- lib/services/auth/bloc/auth_event.dart
+++ lib/services/auth/bloc/auth_event.dart
@@ -12,11 +12,25 @@ class AuthEventInitialize extends AuthEvent {
   const AuthEventInitialize();
 }
 
+class AuthEventSendEmailVerification extends AuthEvent {
+  const AuthEventSendEmailVerification();
+}
+
 // defines the data used to login a user
 class AuthEventLogIn extends AuthEvent {
   final String email;
   final String password;
   const AuthEventLogIn(this.email, this.password);
+}
+
+class AuthEventRegister extends AuthEvent {
+  final String email;
+  final String password;
+  const AuthEventRegister(this.email, this.password);
+}
+
+class AuthEventShouldRegister extends AuthEvent {
+  const AuthEventShouldRegister();
 }
 
 // logout the current user like authservice.firebase().logOut() in notes_view.dart
--- lib/services/auth/bloc/auth_state.dart
+++ lib/services/auth/bloc/auth_state.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:mynotes/services/auth/auth_user.dart';
 
@@ -8,9 +9,13 @@ abstract class AuthState {
   const AuthState();
 }
 
-// login and communication with firebase
-class AuthStateLoading extends AuthState {
-  const AuthStateLoading();
+class AuthStateUninitialized extends AuthState {
+  const AuthStateUninitialized();
+}
+
+class AuthStateRegistering extends AuthState {
+  final Exception? exception;
+  const AuthStateRegistering(this.exception);
 }
 
 // current user
@@ -25,13 +30,14 @@ class AuthStateNeedsVerification extends AuthState {
 }
 
 // logout
-class AuthStateLoggedOut extends AuthState {
+class AuthStateLoggedOut extends AuthState with EquatableMixin {
   final Exception? exception;
-  const AuthStateLoggedOut(this.exception);
-}
+  final bool isLoading;
+  const AuthStateLoggedOut({
+    required this.exception,
+    required this.isLoading,
+  });
 
-// handle logout errors
-class AuthStateLogoutFailure extends AuthState {
-  final Exception exception;
-  const AuthStateLogoutFailure(this.exception);
+  @override
+  List<Object?> get props => [exception, isLoading];
 }
